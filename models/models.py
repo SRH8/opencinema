ffbe6e52245a1410347f1da84f1d1458dfb87157
SRH8 <sergiofraga.cs@gmail.com>
2022-02-25 18:30:20 +0100

    Created film model

--- models/models.py
+++ models/models.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class Film(models.Model):
+    _name = 'opencinema.film'
+    _description = "OpenCinema Films"
 
-# class opencinema(models.Model):
-#     _name = 'opencinema.opencinema'
-#     _description = 'opencinema.opencinema'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
