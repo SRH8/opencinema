64adeb4d13ed08249c255c272605fdaf8e6776b5
SRH8 <sergiofraga.cs@gmail.com>
2022-03-03 08:55:26 +0100

    Modified manifest to consider module as application

--- __manifest__.py
+++ __manifest__.py
@@ -18,6 +18,7 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+    'application': 'true',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
