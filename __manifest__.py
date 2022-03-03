1a4e4c0004bbe546a110790079ae3c0b48e968b1
SRH8 <sergiofraga.cs@gmail.com>
2022-03-03 08:52:23 +0100

    Modified manifest to load demo data

--- __manifest__.py
+++ __manifest__.py
@@ -30,7 +30,7 @@
         'security/ir.model.access.csv',
     ],
     # only loaded in demonstration mode
-    #'demo': [
-     #   'demo/demo.xml',
-    #],
+    'demo': [
+        'demo/demo.xml',
+    ],
 }
