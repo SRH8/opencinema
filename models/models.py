52d1dbcfb8293aa53200a2ca317211ae83f3f87d
SRH8 <sergiofraga.cs@gmail.com>
2022-03-08 09:20:03 +0100

    Added director model

--- models/models.py
+++ models/models.py
@@ -10,3 +10,13 @@ class Film(models.Model):
     description = fields.Text(string="Description")
     duration = fields.Integer(sting="Duration")
     release_date = fields.Char(string="Release date")
+    
+    
+    
+    
+    class Director(models.Model):
+        _name = 'opencinema.director'
+        _description = "OpenCinema Directors"
+        
+        name = fields.Char(string="Name", required=True)
+        nationality = fields.Char(string="Nationality")
